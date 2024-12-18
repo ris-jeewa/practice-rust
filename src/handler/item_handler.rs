550f9956d568ac9ce1e1ddabcd5b809867b036b2
Dheerasekara R J <125817506+ris-jeewa@users.noreply.github.com>
2024-12-18 15:12:23 +0530

    remove unused handlers

--- src/handler/item_handler.rs
+++ src/handler/item_handler.rs
@@ -53,63 +53,6 @@ pub async fn delete_item(
     }
 }
 
-// #[instrument(skip(db, item_data))]
-// pub async fn update_item(
-//     Extension(db): Extension<DatabaseConnection>,
-//     Path(item_id): Path<i32>,
-//     Json(item_data): Json<UpdateItemModel>,
-// ) -> impl IntoResponse {
-//     info!("Updating item with ID: {}", item_id);
-
-//     // Fetch the product to update
-//     let product_result = item::Entity::find_by_id(item_id)
-//         .one(&db)
-//         .await;
-
-//     match product_result {
-//         Ok(Some(existing_item)) => {
-//             let mut updated_item: item::ActiveModel = existing_item.into();
-
-//             updated_item.size = Set(item_data.size);
-//             updated_item.color = Set(item_data.color);
-
-//             updated_item.stock = Set(item_data.stock.unwrap_or(0)); 
-
-//             match updated_item.update(&db).await {
-//                 Ok(_) => {
-//                     info!("Item with ID {} updated successfully", item_id);
-//                     if let Err(e) = db.close().await {
-//                         error!("Failed to close the database connection: {:?}", e);
-//                     }
-//                     (StatusCode::ACCEPTED, "Item updated")
-//                 }
-//                 Err(err) => {
-//                     error!("Failed to update item with ID {}: {:?}", item_id, err);
-//                     (
-//                         StatusCode::INTERNAL_SERVER_ERROR,
-//                         "Failed to update item",
-//                     )
-//                 }
-//             }
-//         }
-//         Ok(None) => {
-//             error!("Item with ID {} not found", item_id);
-//             (
-//                 StatusCode::NOT_FOUND,
-//                 "Item not found",
-//             )
-//         }
-//         Err(err) => {
-//             error!("Failed to fetch item with ID {}: {:?}", item_id, err);
-//             (
-//                 StatusCode::INTERNAL_SERVER_ERROR,
-//                 "Failed to fetch item",
-//             )
-//         }
-//     }
-// }
-
-
 pub async fn update_item(
     State(service): State<ItemService>,
     Path(item_id): Path<i32>,
@@ -143,50 +86,6 @@ pub async fn update_item(
         }
     }
 }
-
-
-// #[instrument(skip(db))]
-// pub async fn get_item_by_id(
-//     Path(id): Path<i32>,                          // Extract the ID from the URL path
-//     Extension(db): Extension<DatabaseConnection>, // Pass the database connection
-// ) -> Response {
-//     info!("Fetching item with ID: {}", id);
-
-//     match item::Entity::find_by_id(id).one(&db).await {
-//         Ok(Some(item)) => {
-//             info!("Successfully fetched item with ID: {}", id);
-            
-//             let json_response = item_model::ItemModel {
-//                 id: item.id,
-//                 product_id: item.product_id,
-//                 color: item.color,
-//                 size: item.size,
-//                 stock: item.stock,
-//             };
-        
-//             Response::builder()
-//                 .status(StatusCode::OK)
-//                 .header(CONTENT_TYPE, "application/json")
-//                 .body(serde_json::to_string(&json_response).unwrap().into())
-//                 .unwrap()
-//         }
-
-//         Ok(None) => {
-//             warn!("Item with ID: {} not found", id);
-//             Response::builder()
-//                 .status(StatusCode::NOT_FOUND)
-//                 .body("Item not found".into())
-//                 .unwrap()
-//         }
-//         Err(err) => {
-//             error!("Failed to retrieve item with ID: {}: {:?}", id, err);
-//             Response::builder()
-//                 .status(StatusCode::INTERNAL_SERVER_ERROR)
-//                 .body("Failed to retrieve item".into())
-//                 .unwrap()
-//         }
-//     }
-// }
 
 pub async fn get_item_by_id(
     State(service): State<ItemService>,
